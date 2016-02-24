7d41d82ac547c3a5fbaa578aadad4acc3e6da37d
Streicher Markus <markus.streicher@cpg.de>
2016-02-24 14:07:38 +0100

    running ssi with thymeleaf

--- src/main/java/de/cpg/sample/SpringSsiApplication.java
+++ src/main/java/de/cpg/sample/SpringSsiApplication.java
@@ -2,9 +2,19 @@ package de.cpg.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.templateresolver.TemplateResolver;
+import org.thymeleaf.templateresolver.UrlTemplateResolver;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class SpringSsiApplication {
+
+	@Bean
+	public TemplateResolver urlTemplateResolver() {
+		return new UrlTemplateResolver();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSsiApplication.class, args);
